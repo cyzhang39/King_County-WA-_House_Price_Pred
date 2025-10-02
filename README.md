fd94383bf5ef82356ac476544212896cd26ed9d5
TimZhang <czhang35@terpail.umd.edu>
2025-10-02 01:16:35 -0400

    revisiting and cleaned code, updated readme

--- README.md
+++ README.md
@@ -2,8 +2,27 @@
 ## Description
 A model trained on Kaggle dataset https://www.kaggle.com/datasets/doesnotcompile/nf-kc-house-data using linear regression, with a flask server and simple HTML/CSS + JS UI.
 ## How to run
-Download all dependencies, then run:
-flask run
-in this project's root directory
+Clone this repo:
+```
+git clone https://github.com/cyzhang39/King_County-WA-_House_Price_Pred.git
+```
+Create activate conda environment
+```
+conda create -n house python=3.9 --y
+conda activate house
+```
+Install flask  
+```
+sudo apt install python3-flask
+```
+
+Install dependencies
+```
+pip install -r requirements.txt
+```
+And finally, run it:
+```
+python -m flask run
+```
 ## Information
-Roughly 93% accuracy
+Roughly 93% training accuracy
--- utils.py
+++ utils.py
@@ -1,32 +1,40 @@
 import json
 import pickle
 import numpy as np
+from pathlib import Path
 
 __model = None
 __cols = None
 __zip = None
 
+_BASE = Path(__file__).resolve().parent
+_MODEL_DIR = _BASE / "model"
+_COLS_PATH = _MODEL_DIR / "cols.json"
+_MODEL_PATH = _MODEL_DIR / "kc_housing_predict"
 
-def get_zip():
-    return __zip
-
-def load_file():
-    global __zip
-    global __cols
-    global __model
-    with open("./model/cols.json", "r") as f:
-        __cols = json.load(f)['data_columns']
+def _ensure_loaded():
+    global __zip, __cols, __model
+    if __cols is not None and __model is not None:
+        return
+    with _COLS_PATH.open("r") as f:
+        __cols = json.load(f)["data_columns"]
         __zip = __cols[8:]
-    with open("./model/kc_housing_predict", 'rb') as f:
+    with _MODEL_PATH.open("rb") as f:
         __model = pickle.load(f)
 
-def price_prediction(zip, bed, bath, sqft, floors, sqft_above, sqft_basement, yr_built, yr_reno):
+def get_zip():
+    _ensure_loaded()
+    return __zip
+
+def price_prediction(zip_code, bed, bath, sqft, floors, sqft_above, sqft_basement, yr_built, yr_reno):
+    _ensure_loaded()
+
     try:
-        i = __cols.index(zip)
-    except:
+        i = __cols.index(str(zip_code))
+    except ValueError:
         i = -1
 
-    a = np.zeros(len(__cols))
+    a = np.zeros(len(__cols), dtype=float)
     a[0] = bed
     a[1] = bath
     a[2] = sqft
@@ -36,16 +44,14 @@ def price_prediction(zip, bed, bath, sqft, floors, sqft_above, sqft_basement, yr
     a[6] = yr_built
     a[7] = yr_reno
     if i >= 0:
-        a[i] = 1
-    result = __model.predict([a])[0]
+        a[i] = 1.0
+
+    result = float(__model.predict([a])[0])
     if result > 300000:
-        result = result - 100000
+        result -= 100000
     return round(result, 2)
 
-
-
 if __name__ == "__main__":
-    load_file()
     print(price_prediction('98001', 3, 4, 2714, 2, 2714, 0, 2005, 2005))
     print(price_prediction('98199', 5, 8, 3520, 2, 3520, 0, 2001, 2001))
     print(price_prediction('98034', 3, 3, 1260, 1, 1260, 0, 1872, 1972))
